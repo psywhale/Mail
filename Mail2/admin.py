dfa4b0f8d1103616f976b5e523769d1a1be8aac2
brian <brian.carpenter@wosc.edu>
2017-11-17 10:25:03 -0600

    setup

--- Mail2/admin.py
+++ Mail2/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ['fk_to', 'read', 'fk_mail']
+
+class MailAdmin(admin.ModelAdmin):
+    list_display = ['fk_sender', 'subject']
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ['filename', 'fk_mail']
 
 # Register your models here.
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Mail, MailAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
--- Mail2/views.py
+++ Mail2/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Route, Mail, Attachment
+from braces.views import LoginRequiredMixin
 
 # Create your views here.
+
+
+class IndexView(LoginRequiredMixin,TemplateView):
+    template_name = 'index.html'
