bc14695a99f5a8931e3d1d797d550589bd9d7f57
brian <brian.carpenter@wosc.edu>
2017-11-17 09:15:46 -0600

    setup

--- Mail2/models.py
+++ Mail2/models.py
@@ -8,10 +8,16 @@ class Mail(models.Model):
     subject = models.CharField(max_length=512)
     fk_sender = models.ForeignKey(User)
 
+    def __str__(self):
+        return self.subject
+
 class Attachment(models.Model):
     filepath = models.CharField(max_length=512)
     filename = models.CharField(max_length=1024)
     fk_mail = models.ForeignKey(Mail)
+
+    def __str__(self):
+        return self.filename
 
 class Route(models.Model):
     fk_to = models.ForeignKey(User)
