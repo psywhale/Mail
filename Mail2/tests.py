9b05d5e6da2df48194fb8da7d6e155ff93e3dd5d
brian <brian.carpenter@wosc.edu>
2017-12-01 09:01:53 -0600

    reply view

--- Mail2/tests.py
+++ Mail2/tests.py
@@ -134,17 +134,129 @@ class InboxTest(TestCase):
         print(reslogin.context['courses'])
         self.assertTrue(allUnique(reslogin.context['courses']))
 
-    def test_can_see_individual_mail(self):
+
+
+
+class ReplyTest(TestCase):
+
+    def setUp(self):
+
+        frank = User.objects.create_user(username='Frank',
+                                         password='whatevs',
+                                         first_name='Testislez',
+                                         last_name='Person')
+        ned = User.objects.create_user(username='Ned',
+                                       password='whatevas',
+                                       first_name='Ned',
+                                       last_name='Person')
+        # Message with an attachment that is for Frank
+        mailmsg = Mail.objects.create(content='Test Message',
+                                      subject='test message for Frank',
+                                      fk_sender=ned,
+                                      termcode="172s",
+                                      section="21232")
+
+        Route.objects.create(fk_to=frank,
+                             read=False,
+                             fk_mail=mailmsg)
+
+        Attachment.objects.create(filepath='/mnt/maildata',
+                                  filename='tatadfa.doc',
+                                  fk_mail=mailmsg
+                                  )
+        # Messsage for frank that does not have an attachment
+        mailmsg = Mail.objects.create(content='Test message for frank',
+                                      subject='No Attachment for frank',
+                                      fk_sender=ned,
+                                      termcode = "172s",
+                                      section = "21231")
+
+        Route.objects.create(fk_to=frank,
+                             read=False,
+                             fk_mail=mailmsg)
+        # Message for "Ned"
+        mailmsg = Mail.objects.create(content='Test Message 3',
+                                      subject='test subject for Ned',
+                                      fk_sender=frank,
+                                      termcode="172s",
+                                      section="21232"
+                                      )
+
+        Route.objects.create(fk_to=ned,
+                             read=False,
+                             fk_mail=mailmsg)
+
+        Attachment.objects.create(filepath='/mnt/maildata',
+                                  filename='tatadfa2.doc',
+                                  fk_mail=mailmsg
+                                  )
+
+        # A Message to frank that is read
+        mailmsg = Mail.objects.create(content='Test Message 4',
+                                      subject='test email read',
+                                      fk_sender=ned,
+                                      termcode="172s",
+                                      section="21231"
+                                      )
+
+        Route.objects.create(fk_to=frank,
+                             read=True,
+                             fk_mail=mailmsg)
+
+        Attachment.objects.create(filepath='/mnt/maildata',
+                                  filename='tatadfa3.doc',
+                                  fk_mail=mailmsg
+                                  )
+
+    def test_user_can_see_mail(self):
+        c = Client()
+        mailmsg = Mail.objects.create(content='Test REPLY',
+                                      subject='test timestamp for Ned',
+                                      fk_sender=User.objects.get(username="Ned"),
+                                      termcode="172s",
+                                      section="21231",
+
+                                      )
+        route = Route.objects.create(fk_to=User.objects.get(username="Frank"),
+                                     read=False,
+                                     fk_mail=mailmsg)
+        res = c.login(username='Frank', password='whatevs')
+        reslogin = c.get('/reply/' + str(mailmsg.id), follow=True)
+        self.assertEqual(reslogin.status_code, 200)
+
+
+    def test_user_cannot_see_other_users_mail(self):
         c = Client()
         mailmsg = Mail.objects.create(content='Test REPLY',
                                       subject='test timestamp for Ned',
                                       fk_sender=User.objects.get(username="Frank"),
                                       termcode="172s",
                                       section="21231",
-                                      fk_to=User.objects.get(username="Ned")
+
                                       )
+        route = Route.objects.create(fk_to=User.objects.get(username="Ned"),
+                                     read=False,
+                                     fk_mail=mailmsg)
         res = c.login(username='Frank', password='whatevs')
-        reslogin = c.get('/read/'+mailmsg.id)
+        reslogin = c.get('/reply/' + str(mailmsg.id), follow=True)
+        self.assertEqual(reslogin.status_code, 403)
+
+    def test_message_gets_marked_as_read_on_open(self):
+        c = Client()
+        mailmsg = Mail.objects.create(content='Test REPLY',
+                                      subject='test timestamp for Ned',
+                                      fk_sender=User.objects.get(username="Frank"),
+                                      termcode="172s",
+                                      section="21231",
+
+                                      )
+        route = Route.objects.create(fk_to=User.objects.get(username="Ned"),
+                                     read=False,
+                                     fk_mail=mailmsg)
+        res = c.login(username='Ned', password='whatevas')
+        reslogin = c.get('/reply/' + str(mailmsg.id), follow=True)
+        self.assertTrue(Route.objects.get(fk_mail=mailmsg).read)
+
 
 
 def allUnique(x):
