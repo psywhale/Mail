b6876070555c2e8dddec078b3ac436c2133dcc93
brian <brian.carpenter@wosc.edu>
2017-12-08 15:25:16 -0600

    sending message stuff

--- Mail2/urls.py
+++ Mail2/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import IndexView,ReplyView, LabelView, ListUnreadView
+from .views import IndexView,ReplyView, LabelView, ListUnreadView, ArchiveMailView
 
 urlpatterns= [
     url(r'^$',IndexView.as_view(), name='IndexView'),
@@ -7,4 +7,5 @@ urlpatterns= [
     url(r'^label/(?P<sn>[0-9Cc]{5}\-[0-9]{3}[sS]+)/$', LabelView.as_view(), name='label'),
     url(r'^listunread/$', ListUnreadView.as_view(), name='listunread'),
     url(r'^audit/$', ListUnreadView.as_view(), name='listunread'),
+    url(r'^archive/$', ArchiveMailView.as_view(), name='archivemail'),
 ]
